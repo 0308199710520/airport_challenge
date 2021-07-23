9dfe3e465a36e901ef7f50caa422e9f25025e7ac
Michael Poole <michaelpoole10@gmail.com>
2021-07-23 14:29:16 +0100

    a little stub testing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,20 +4,17 @@ describe "Airport" do
   describe "#land" do
     it "should land and store a single plane in the airport" do
       airport = Airport.new
-      airport.weather_set("sunny")
       airport.land(Plane.new)
       expect(airport.empty_hanger?).to(eq(false))
     end 
     it "should return an error when you try to land a plane in a full hanger" do
       airport = Airport.new()
-      airport.weather_set("sunny")
       20.times { airport.land(Plane.new) }
       expect { airport.land(Plane.new) }.to(raise_error("The hanger is full!"))
     end
 
     it "should allow you to pass an adjusted capacity" do
       airport = Airport.new(50)
-      airport.weather_set("sunny")
       airport.capacity.times { airport.land(Plane.new) }
       expect { airport.land(Plane.new) }.to(raise_error("The hanger is full!"))
     end
@@ -33,7 +30,6 @@ describe "Airport" do
   describe "#takeoff" do
     it "should return true when empty? is called after takeoff" do
       airport = Airport.new
-      airport.weather_set("sunny")
       airport.land(Plane.new)
       airport.takeoff
       expect(airport.empty_hanger?).to(eq(true))
@@ -41,7 +37,6 @@ describe "Airport" do
 
     it "should not allow takeoff when weather is stormy" do
       airport = Airport.new
-      airport.weather_set("sunny")
       airport.land(Plane.new)
       airport.weather_set("stormy") 
       expect { airport.takeoff }.to(raise_error("The weather is too rough to takeoff"))
